bdf4043ea3b86f818a77ceb4754927e8f1d283fe
dkehn <dekehn@gmail.com>
2022-09-16 19:53:31 +0000

    Adding more operator infrastructure

---
+++ api/v1beta1/designateapi_types.go
@@ -0,0 +1,214 @@
+/*
+Copyright 2022.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1beta1
+
+import (
+	"fmt"
+
+	"github.com/openstack-k8s-operators/lib-common/modules/common/condition"
+	"github.com/openstack-k8s-operators/lib-common/modules/common/endpoint"
+
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+const (
+	// DbSyncHash hash
+	DbSyncHash = "dbsync"
+
+	// DeploymentHash hash used to detect changes
+	DeploymentHash = "deployment"
+)
+
+// Hash - struct to add hashes to status
+type Hash struct {
+	// Name of hash referencing the parameter
+	Name string `json:"name,omitempty"`
+	// Hash
+	Hash string `json:"hash,omitempty"`
+}
+
+// DesignateAPISpec defines the desired state of DesignateAPI
+type DesignateAPISpec struct {
+	// +kubebuilder:validation:Optional
+	// +kubebuilder:default=designate
+	// ServiceUser - optional username used for this service to register in designate
+	ServiceUser string `json:"serviceUser"`
+
+	// +kubebuilder:validation:Required
+	// MariaDB instance name
+	// Right now required by the maridb-operator to get the credentials from the instance to create the DB
+	// Might not be required in future
+	DatabaseInstance string `json:"databaseInstance,omitempty"`
+
+	// +kubebuilder:validation:Optional
+	// +kubebuilder:default=designate
+	// DatabaseUser - optional username used for designate DB, defaults to designate
+	// TODO: -> implement needs work in mariadb-operator, right now only designate
+	DatabaseUser string `json:"databaseUser"`
+
+	ContainerImage string `json:"containerImage,omitempty"`
+
+	// +kubebuilder:validation:Optional
+	// +kubebuilder:default=1
+	// +kubebuilder:validation:Maximum=32
+	// +kubebuilder:validation:Minimum=0
+	// Replicas of designate API to run
+	Replicas int32 `json:"replicas"`
+
+	// +kubebuilder:validation:Required
+	// Secret containing OpenStack password information for designate DesignatePassword NovaPassword
+	Secret string `json:"secret,omitempty"`
+
+	// +kubebuilder:validation:Optional
+	// PasswordSelectors - Selectors to identify the DB and AdminUser password from the Secret
+	PasswordSelectors PasswordSelector `json:"passwordSelectors,omitempty"`
+
+	// +kubebuilder:validation:Optional
+	// NodeSelector to target subset of worker nodes running this service
+	NodeSelector map[string]string `json:"nodeSelector,omitempty"`
+
+	// +kubebuilder:validation:Optional
+	// Debug - enable debug for different deploy stages. If an init container is used, it runs and the
+	// actual action pod gets started with sleep infinity
+	Debug DesignateAPIDebug `json:"debug,omitempty"`
+
+	// +kubebuilder:validation:Optional
+	// +kubebuilder:default=false
+	// PreserveJobs - do not delete jobs after they finished e.g. to check logs
+	PreserveJobs bool `json:"preserveJobs,omitempty"`
+
+	// +kubebuilder:validation:Optional
+	// +kubebuilder:default="# add your customization here"
+	// CustomServiceConfig - customize the service config using this parameter to change service defaults,
+	// or overwrite rendered information using raw OpenStack config format. The content gets added to
+	// to /etc/<service>/<service>.conf.d directory as custom.conf file.
+	CustomServiceConfig string `json:"customServiceConfig,omitempty"`
+
+	// +kubebuilder:validation:Optional
+	// ConfigOverwrite - interface to overwrite default config files like e.g. logging.conf or policy.json.
+	// But can also be used to add additional files. Those get added to the service config dir in /etc/<service> .
+	// TODO: -> implement
+	DefaultConfigOverwrite map[string]string `json:"defaultConfigOverwrite,omitempty"`
+
+	// +kubebuilder:validation:Optional
+	// Resources - Compute Resources required by this service (Limits/Requests).
+	// https://kubernetes.io/docs/concepts/configuration/manage-resources-containers/
+	Resources corev1.ResourceRequirements `json:"resources,omitempty"`
+
+	// +kubebuilder:validation:Required
+	// ovn-connection configmap which holds NBConnection and SBConnection string
+	OVNConnectionConfigMap string `json:"ovnConnectionConfigMap,omitempty"`
+}
+
+// PasswordSelector to identify the DB and AdminUser password from the Secret
+type PasswordSelector struct {
+	// +kubebuilder:validation:Optional
+	// +kubebuilder:default="DesignateDatabasePassword"
+	// Database - Selector to get the designate database user password from the Secret
+	// TODO: not used, need change in mariadb-operator
+	Database string `json:"database,omitempty"`
+	// +kubebuilder:validation:Optional
+	// +kubebuilder:default="DesignatePassword"
+	// Database - Selector to get the designate service password from the Secret
+	Service string `json:"admin,omitempty"`
+	// +kubebuilder:validation:Optional
+	// +kubebuilder:default="NovaPassword"
+	// Database - Selector to get the nova service password from the Secret
+	NovaService string `json:"novaadmin,omitempty"`
+}
+
+// DesignateAPIDebug defines the observed state of DesignateAPIDebug
+type DesignateAPIDebug struct {
+	// +kubebuilder:validation:Optional
+	// +kubebuilder:default=false
+	// DBSync enable debug
+	DBSync bool `json:"dbSync,omitempty"`
+	// +kubebuilder:validation:Optional
+	// +kubebuilder:default=false
+	// Bootstrap enable debug
+	Bootstrap bool `json:"bootstrap,omitempty"`
+	// +kubebuilder:validation:Optional
+	// +kubebuilder:default=false
+	// Service enable debug
+	Service bool `json:"service,omitempty"`
+}
+
+// DesignateAPIStatus defines the observed state of DesignateAPI
+type DesignateAPIStatus struct {
+	// ReadyCount of designate API instances
+	ReadyCount int32 `json:"readyCount,omitempty"`
+
+	// Map of hashes to track e.g. job status
+	Hash map[string]string `json:"hash,omitempty"`
+
+	// API endpoint
+	APIEndpoints map[string]string `json:"apiEndpoint,omitempty"`
+
+	// Conditions
+	Conditions condition.Conditions `json:"conditions,omitempty" optional:"true"`
+
+	// Designate Database Hostname
+	DatabaseHostname string `json:"databaseHostname,omitempty"`
+
+	// ServiceID - the ID of the registered service in keystone
+	ServiceID string `json:"serviceID,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+// +kubebuilder:subresource:status
+// +kubebuilder:printcolumn:name="Status",type="string",JSONPath=".status.conditions[0].status",description="Status"
+// +kubebuilder:printcolumn:name="Message",type="string",JSONPath=".status.conditions[0].message",description="Message"
+
+// DesignateAPI is the Schema for the designateapis API
+type DesignateAPI struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   DesignateAPISpec   `json:"spec,omitempty"`
+	Status DesignateAPIStatus `json:"status,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+
+// DesignateAPIList contains a list of DesignateAPI
+type DesignateAPIList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []DesignateAPI `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&DesignateAPI{}, &DesignateAPIList{})
+}
+
+// GetEndpoint - returns OpenStack endpoint url for type
+func (instance DesignateAPI) GetEndpoint(endpointType endpoint.Endpoint) (string, error) {
+	if url, found := instance.Status.APIEndpoints[string(endpointType)]; found {
+		return url, nil
+	}
+	return "", fmt.Errorf("%s endpoint not found", string(endpointType))
+}
+
+// IsReady - returns true if service is ready to server requests
+func (instance DesignateAPI) IsReady() bool {
+	// Ready when:
+	// the service is registered in keystone
+	// AND
+	// there is at least a single pod to serve the designate API service
+	return instance.Status.ServiceID != "" && instance.Status.ReadyCount >= 1
+}
